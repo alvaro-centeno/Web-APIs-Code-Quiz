3e8a21588743885f32818062df96d42f956c4812
Alvaro Centeno <alvaroc0689@gmail.com>
2020-04-27 00:04:48 -0700

    added function to start game and function to run through the different questions

--- app.js
+++ app.js
@@ -32,6 +32,84 @@ var questions = [
         choice3: "shoulder shrug",
         choice4: "yes",
         answer: 4
+    },
+    {
+        question: "Inside which HTML element do we put the Javascript?",
+        choice1: "<script>",
+        choice2: "<javascript>",
+        choice3: "<js>",
+        choice4: "<scripting>",
+        answer: 1
+    },
+    {
+        question: "IS this question 2?",
+        choice1: "maybe",
+        choice2: "yes",
+        choice3: "no",
+        choice4: "maybe-so",
+        answer: 2
+    },
+    {
+        question: "Are you going crazy?",
+        choice1: "no",
+        choice2: "maybe",
+        choice3: "shoulder shrug",
+        choice4: "yes",
+        answer: 4
+    },
+    {
+        question: "Inside which HTML element do we put the Javascript?",
+        choice1: "<script>",
+        choice2: "<javascript>",
+        choice3: "<js>",
+        choice4: "<scripting>",
+        answer: 1
+    },
+    {
+        question: "IS this question 2?",
+        choice1: "maybe",
+        choice2: "yes",
+        choice3: "no",
+        choice4: "maybe-so",
+        answer: 2
+    },
+    {
+        question: "Are you going crazy?",
+        choice1: "no",
+        choice2: "maybe",
+        choice3: "shoulder shrug",
+        choice4: "yes",
+        answer: 4
+    },
+    {
+        question: "Are you going crazy?",
+        choice1: "no",
+        choice2: "maybe",
+        choice3: "shoulder shrug",
+        choice4: "yes",
+        answer: 4
     }
 ];
 
+var correctBonus = 10;
+var maxQuestions = 10;
+
+
+function startGame() {
+    questionNumber = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+function getNewQuestion() {
+    questionNumber++;
+    questionNumberText.innerText = questionNumber + "/" + maxQuestions;
+    var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(function (choice) {
+        var number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
