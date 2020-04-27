4960953b2603199469e01c2704c1d532726b4396
Alvaro Centeno <alvaroc0689@gmail.com>
2020-04-27 13:55:43 -0700

    added js that pulls high scores from local storage and displays them on the screen.

--- highscores.js
+++ highscores.js
@@ -1,3 +1,10 @@
 var highScoresList = document.getElementById("highScoresList");
 var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+var li = document.createElement("li");
+
+highScoresList.innerHTML = highScores
+    .map(score => {
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    })
+    .join("");
 
